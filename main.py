4e7090841523cc7253fe51ec4583fb239482259d
Robert Cojocariu <robycojocariu9@gmail.com>
2023-01-13 18:55:01 +0200

    final lab

--- main.py
+++ main.py
@@ -2,9 +2,22 @@ from domain import Grammar, Parser, ParserOutput
 from domain.parser import Error
 
 
+def get_sequence(file_name):
+    file = open(file_name, "r")
+    lines = file.readlines()
+    sequence = []
+
+    for line in lines:
+        elements = line.split("|")
+        if elements[0] != 'TOKEN':
+            sequence.append(elements[0])
+
+    return "".join(sequence)
+
+
 if __name__ == "__main__":
-    grammar = Grammar("./inputs/g1.txt")
-    word = "xaaacbc"
+    grammar = Grammar("./inputs/g2.txt")
+    word = get_sequence("./inputs/pif.txt")
     output_file = "./outputs/error.out"
     try:
         parser = Parser(grammar, word)
@@ -15,4 +28,3 @@ if __name__ == "__main__":
         file = open(output_file, "w")
         file.write(f"Sequence {word} not accepted!")
         file.close()
-
